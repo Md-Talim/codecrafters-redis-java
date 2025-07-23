13438313ff6fa5cdfb723b885463106e4d3e6c77
Mohammad Talim <talimmohammad116@gmail.com>
2025-07-23 21:23:07 +0530

    respond to PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,21 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-    public static void main(String[] args) {
-        ServerSocket serverSocket;
-        Socket clientSocket = null;
-        int port = 6379;
+    public static final int PORT = 6379;
 
-        try {
-            serverSocket = new ServerSocket(port);
+    public static void main(String[] args) {
+        Socket clientSocket = null;
+
+        try (final var serverSocket = new ServerSocket(PORT)) {
             serverSocket.setReuseAddress(true);
             clientSocket = serverSocket.accept();
+
+            OutputStream outputStream = clientSocket.getOutputStream();
+            outputStream.write("+PONG\r\n".getBytes());
+            outputStream.flush();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
