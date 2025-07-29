f01e37ef04606e81dbd34c8491dddaccc267602b
Mohammad Talim <talimmohammad116@gmail.com>
2025-07-29 19:50:21 +0530

    feat: implement timeout handling in XREAD command

--- src/main/java/redis/command/stream/XReadCommand.java
+++ src/main/java/redis/command/stream/XReadCommand.java
@@ -1,5 +1,6 @@
 package redis.command.stream;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,7 @@ import redis.resp.type.RArray;
 import redis.resp.type.RValue;
 import redis.store.Storage;
 import redis.stream.Stream;
+import redis.stream.StreamEntry;
 import redis.stream.identifier.Identifier;
 
 public class XReadCommand implements Command {
@@ -23,10 +25,19 @@ public class XReadCommand implements Command {
         record Query(String key, Identifier identifier) {
         }
         List<Query> queries = new ArrayList<Query>();
+        Duration timeout = null;
 
         int size = args.size();
         for (int i = 0; i < size; i++) {
             String element = args.get(i).toString();
+
+            if ("block".equalsIgnoreCase(element)) {
+                ++i;
+
+                element = args.get(i).toString();
+                timeout = Duration.ofMillis(Long.parseLong(element));
+                continue;
+            }
 
             if ("streams".equalsIgnoreCase(element)) {
                 ++i;
@@ -35,12 +46,33 @@ public class XReadCommand implements Command {
                 int offset = remaining / 2;
                 for (int j = 0; j < offset; j++) {
                     String key = args.get(i + j).toString();
-                    Identifier identifier = Identifier.parse(args.get(i + offset + j).toString());
+                    element = args.get(i + offset + j).toString();
+                    Identifier identifier = timeout != null && "$".equals(element) ? null : Identifier.parse(element);
                     queries.add(new Query(key, identifier));
                 }
 
                 break;
             }
+        }
+
+        if (timeout != null) {
+            Query query = queries.getFirst();
+            String key = query.key();
+            Stream stream = (Stream) storage.get(key);
+            List<StreamEntry> entries = stream.read(query.identifier(), timeout);
+
+            if (entries == null) {
+                return new BulkString(null);
+            }
+
+            List<RValue> entryResponse = new ArrayList<>();
+            for (var entry : entries) {
+                var entryData = List.of(new BulkString(entry.identifier().toString()), new RArray(entry.content()));
+                entryResponse.add(new RArray(entryData));
+            }
+
+            entryResponse = List.of(new BulkString(key), new RArray(entryResponse));
+            return new RArray(List.of(new RArray(entryResponse)));
         }
 
         List<RValue> fullResponse = new ArrayList<RValue>();
--- src/main/java/redis/stream/Stream.java
+++ src/main/java/redis/stream/Stream.java
@@ -1,7 +1,12 @@
 package redis.stream;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import redis.resp.type.RValue;
 import redis.stream.identifier.Identifier;
@@ -11,82 +16,137 @@ import redis.stream.identifier.WildcardIdentifier;
 
 public class Stream {
     private final List<StreamEntry> entries = new ArrayList<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Condition dataCondition = lock.writeLock().newCondition();
+
     private final String XADD_ID_EQUAL_OR_SMALLER = "ERR The ID specified in XADD is equal or smaller than the target stream top item";
     private final String XADD_ID_GREATER_THAN_ZERO = "ERR The ID specified in XADD must be greater than 0-0";
 
-    public synchronized UniqueIdentifier add(Identifier id, List<RValue> content) {
-        var unique = switch (id) {
-            case MillisecondsIdentifier identifier -> getIdentifier(identifier.milliseconds());
-            case UniqueIdentifier identifier -> identifier;
-            case WildcardIdentifier _ -> getIdentifier(System.currentTimeMillis());
-        };
+    public UniqueIdentifier add(Identifier id, List<RValue> content) {
+        lock.writeLock().lock();
+        try {
+            var unique = switch (id) {
+                case MillisecondsIdentifier identifier -> getIdentifier(identifier.milliseconds());
+                case UniqueIdentifier identifier -> identifier;
+                case WildcardIdentifier _ -> getIdentifier(System.currentTimeMillis());
+            };
 
-        if (unique.milliseconds() == 0 && unique.sequenceNumber() == 0) {
-            throw new RuntimeException(XADD_ID_GREATER_THAN_ZERO);
+            if (unique.milliseconds() == 0 && unique.sequenceNumber() == 0) {
+                throw new RuntimeException(XADD_ID_GREATER_THAN_ZERO);
+            }
+            if (!isUnique(unique)) {
+                throw new RuntimeException(XADD_ID_EQUAL_OR_SMALLER);
+            }
+
+            entries.add(new StreamEntry(unique, content));
+            dataCondition.signalAll();
+
+            return unique;
+        } finally {
+            lock.writeLock().unlock();
         }
-
-        if (!isUnique(unique)) {
-            throw new RuntimeException(XADD_ID_EQUAL_OR_SMALLER);
-        }
-
-        entries.add(new StreamEntry(unique, content));
-
-        return unique;
     }
 
-    public synchronized List<StreamEntry> read(Identifier fromExclusive) {
-        List<StreamEntry> result = new ArrayList<StreamEntry>();
-        boolean collecting = false;
+    public List<StreamEntry> read(Identifier fromExclusive) {
+        lock.readLock().lock();
 
-        for (StreamEntry entry : entries) {
-            var identifier = entry.identifier();
+        try {
+            List<StreamEntry> result = new ArrayList<StreamEntry>();
+            boolean collecting = false;
 
-            if (collecting) {
-                result.add(entry);
-            } else if (identifier.compareTo(fromExclusive) > 0) {
-                collecting = true;
-                result.add(entry);
+            for (StreamEntry entry : entries) {
+                var identifier = entry.identifier();
+
+                if (collecting) {
+                    result.add(entry);
+                } else if (identifier.compareTo(fromExclusive) > 0) {
+                    collecting = true;
+                    result.add(entry);
+                }
             }
-        }
 
-        return result;
+            return result;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
-    public synchronized List<StreamEntry> range(Identifier from, Identifier to) {
-        List<StreamEntry> result = new ArrayList<StreamEntry>();
-        boolean collecting = false;
-
-        for (StreamEntry entry : entries) {
-            var identifier = entry.identifier();
-            if (identifier.compareTo(to) > 0) {
-                break;
-            }
-
-            if (collecting) {
-                result.add(entry);
-            } else if (identifier.compareTo(from) >= 0) {
-                collecting = true;
-                result.add(entry);
+    public List<StreamEntry> read(Identifier fromExclusive, Duration timeout) {
+        if (fromExclusive == null) {
+            fromExclusive = getIdentifier(System.currentTimeMillis());
+        } else {
+            List<StreamEntry> result = read(fromExclusive);
+            if (!result.isEmpty()) {
+                return result;
             }
         }
 
-        return result;
+        if (awaitNewData(timeout)) {
+            return read(fromExclusive);
+        }
+
+        return null;
+    }
+
+    private boolean awaitNewData(Duration timeout) {
+        lock.writeLock().lock();
+        try {
+            return dataCondition.await(timeout.toMillis(), TimeUnit.MILLISECONDS);
+        } catch (InterruptedException ignored) {
+            ;
+        } finally {
+            lock.writeLock().unlock();
+        }
+        return false;
+    }
+
+    public List<StreamEntry> range(Identifier from, Identifier to) {
+        lock.readLock().lock();
+
+        try {
+            List<StreamEntry> result = new ArrayList<StreamEntry>();
+            boolean collecting = false;
+
+            for (StreamEntry entry : entries) {
+                var identifier = entry.identifier();
+                if (identifier.compareTo(to) > 0) {
+                    break;
+                }
+
+                if (collecting) {
+                    result.add(entry);
+                } else if (identifier.compareTo(from) >= 0) {
+                    collecting = true;
+                    result.add(entry);
+                }
+            }
+
+            return result;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public UniqueIdentifier getIdentifier(long milliseconds) {
-        if (!entries.isEmpty()) {
-            var last = entries.getLast().identifier();
-            if (last.milliseconds() == milliseconds) {
-                return new UniqueIdentifier(milliseconds, last.sequenceNumber() + 1);
+        lock.readLock().lock();
+
+        try {
+            if (!entries.isEmpty()) {
+                var last = entries.getLast().identifier();
+                if (last.milliseconds() == milliseconds) {
+                    return new UniqueIdentifier(milliseconds, last.sequenceNumber() + 1);
+                }
             }
+
+            long sequenceNumber = milliseconds == 0 ? 1l : 0l;
+
+            return new UniqueIdentifier(milliseconds, sequenceNumber);
+        } finally {
+            lock.readLock().unlock();
         }
-
-        long sequenceNumber = milliseconds == 0 ? 1l : 0l;
-
-        return new UniqueIdentifier(milliseconds, sequenceNumber);
     }
 
-    public boolean isUnique(UniqueIdentifier identifier) {
+    private boolean isUnique(UniqueIdentifier identifier) {
         if (entries.isEmpty()) {
             return true;
         }
