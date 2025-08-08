1623b55cd5ab06c97fe5f369119cf86299102b29
Mohammad Talim <talimmohammad116@gmail.com>
2025-08-08 23:17:08 +0530

    chore: include TrackedInputStream file

---
+++ src/main/java/redis/util/TrackedInputStream.java
@@ -0,0 +1,37 @@
+package redis.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TrackedInputStream extends InputStream {
+
+    private final InputStream delegate;
+    private long read;
+
+    public TrackedInputStream(InputStream inputStream) {
+        this.delegate = inputStream;
+    }
+
+    @Override
+    public int available() throws IOException {
+        return delegate.available();
+    }
+
+    @Override
+    public int read() throws IOException {
+        int value = delegate.read();
+        if (value != -1) {
+            ++read;
+        }
+
+        return value;
+    }
+
+    public void begin() {
+        read = 0;
+    }
+
+    public long count() {
+        return read;
+    }
+}
